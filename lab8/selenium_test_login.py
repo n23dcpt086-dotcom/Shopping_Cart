774838f60e0d835d42833f275208e089a2751c22
n23dcpt086-dotcom <n23dcpt086@student.ptithcm.edu.vn>
2025-09-29 14:31:13 +0700

    Update selenium_test_login.py

--- lab8/selenium_test_login.py
+++ lab8/selenium_test_login.py
@@ -1,49 +1,37 @@
+import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-
 
 @pytest.fixture(scope="module")
 def driver():
     driver = webdriver.Chrome()
-    driver.get("file:///C:/Users/ADMIN/Shopping_Cart/LAB04/LAB04.html")
+    driver.get("file:///D:/lab4.html")
+    driver.maximize_window()
     yield driver
     driver.quit()
 
+def login(driver, username, password):
+    driver.find_element(By.ID, "username").clear()
+    driver.find_element(By.ID, "password").clear()
+
+    driver.find_element(By.ID, "username").send_keys(username)
+    driver.find_element(By.ID, "password").send_keys(password)
+
+    driver.find_element(By.CSS_SELECTOR, "button.primary").click()
+    time.sleep(1)  # chờ JS xử lý
+
+    message = driver.find_element(By.ID, "message").text
+    return message
 
 def test_login_success(driver):
-    driver.find_element(By.ID, "username").clear()
-    driver.find_element(By.ID, "password").clear()
+    msg = login(driver, "admin", "admin123")
+    assert "Đăng nhập thành công" in msg
 
-    driver.find_element(By.ID, "username").send_keys("admin")
-    driver.find_element(By.ID, "password").send_keys("admin123")
-    driver.find_element(By.CSS_SELECTOR, ".btn.primary").click()
+def test_login_fail(driver):
+    msg = login(driver, "admin", "123456")
+    assert msg == "Tên người dùng hoặc Mật khẩu không đúng."
 
-    time.sleep(1)
-    message = driver.find_element(By.ID, "message").text
-    assert "Đăng nhập thành công" in message
-
-
-def test_login_wrong_password(driver):
-    driver.find_element(By.ID, "username").clear()
-    driver.find_element(By.ID, "password").clear()
-
-    driver.find_element(By.ID, "username").send_keys("wronguser")
-    driver.find_element(By.ID, "password").send_keys("123456")
-    driver.find_element(By.CSS_SELECTOR, ".btn.primary").click()
-
-    time.sleep(1)
-    message = driver.find_element(By.ID, "message").text
-    assert "không đúng" in message
-
-
-def test_login_empty_input(driver):
-    driver.find_element(By.ID, "username").clear()
-    driver.find_element(By.ID, "password").clear()
-
-    driver.find_element(By.CSS_SELECTOR, ".btn.primary").click()
-
-    time.sleep(1)
-    message = driver.find_element(By.ID, "message").text
-    assert "Vui lòng nhập đầy đủ" in message
+def test_login_empty(driver):
+    msg = login(driver, "", "")
+    assert msg == "Vui lòng nhập đầy đủ Tên người dùng và Mật khẩu."
