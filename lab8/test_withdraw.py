5043e0c836f1d650dad56087b40bbe4513e55569
n23dcpt086-dotcom <n23dcpt086@student.ptithcm.edu.vn>
2025-10-04 21:40:03 +0700

    Update test_withdraw.py

--- lab8/test_withdraw.py
+++ lab8/test_withdraw.py
@@ -1,19 +1,17 @@
 import pytest
 from atm_module import verify_pin, withdraw
 
-# ---- TEST verify_pin ----
 def test_verify_pin_correct():
-    assert verify_pin("1234", "1234") == True
+    assert verify_pin("1234", "1234") is True
 
 def test_verify_pin_incorrect():
-    assert verify_pin("1111", "1234") == False
+    assert verify_pin("0000", "1234") is False
 
-# ---- TEST withdraw ----
 def test_withdraw_success():
     assert withdraw(1000, 200) == 800
 
 def test_withdraw_insufficient():
-    assert withdraw(500, 1000) == "Insufficient funds"
+    assert withdraw(50, 100) == "Insufficient funds"
 
 def test_withdraw_invalid_amount():
     with pytest.raises(ValueError):
